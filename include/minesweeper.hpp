4402e3d18a210729e0d7c0e0544318b59e59eba1
DevNielson <212224575+DevNielson@users.noreply.github.com>
2026-02-26 11:31:53 -0300

    .

--- include/minesweeper.hpp
+++ include/minesweeper.hpp
@@ -5,8 +5,9 @@ class Minesweeper
 private:
     std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> m_sprites;
 
+private:
     std::array<SDL_FRect, 8> m_spritesCut;
-    std::array<SDL_FRect, 4> m_spritesPosition;
+    std::array<SDL_FRect, 9> m_spritesPositions;
 
 public:
     Minesweeper();
--- src/minesweeper.cpp
+++ src/minesweeper.cpp
@@ -2,22 +2,30 @@
 
 Minesweeper::Minesweeper()
     : m_sprites { nullptr, SDL_DestroyTexture },
-      m_spritesCut { SDL_FRect
-          { .x { Sprites::SIZE * 0 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 1 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 2 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 3 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 0 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 1 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 2 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Sprites::SIZE * 3 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } }
+      m_spritesCut
+      {
+          SDL_FRect { .x { Sprites::SIZE * 0 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 1 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 2 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 3 }, .y { Sprites::SIZE * 0 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 0 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 1 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 2 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Sprites::SIZE * 3 }, .y { Sprites::SIZE * 1 }, .w { Sprites::SIZE }, .h { Sprites::SIZE } }
       },
-      m_spritesPosition { SDL_FRect
-          { .x {}, .y {}, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Window::WIDTH - Sprites::SIZE }, .y {}, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x {}, .y { Window::HEIGHT - Sprites::SIZE }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
-          { .x { Window::WIDTH - Sprites::SIZE }, .y { Window::HEIGHT - Sprites::SIZE }, .w { Sprites::SIZE },
-              .h { Sprites::SIZE } }
+      m_spritesPositions
+      {
+          SDL_FRect { .x {}, .y {}, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Window::WIDTH - Sprites::SIZE }, .y {}, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x {}, .y { Window::HEIGHT - Sprites::SIZE }, .w { Sprites::SIZE }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Window::WIDTH - Sprites::SIZE }, .y { Window::HEIGHT - Sprites::SIZE }, .w { Sprites::SIZE },
+                      .h { Sprites::SIZE } },
+
+          SDL_FRect { .x {}, .y {}, .w { Window::WIDTH }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { 32.0f }, .y {  100.0f }, .w { Window::WIDTH - Sprites::SIZE * 2 }, .h { Sprites::SIZE } },
+          SDL_FRect { .x {}, .y { Window::HEIGHT - Sprites::SIZE }, .w { Window::WIDTH }, .h { Sprites::SIZE } },
+          SDL_FRect { .x { Window::WIDTH - 32.0f }, .y {}, .w { Sprites::SIZE }, .h { 132.0f } },
+          SDL_FRect { .x {}, .y { Window::WIDTH }, .w { Sprites::SIZE }, .h { Sprites::SIZE } }
       }
 {}
 
@@ -48,8 +56,16 @@ void Minesweeper::Update()
 
 void Minesweeper::Draw(SDL_Renderer *renderer) const
 {
+    SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(4), &m_spritesPositions.at(4));
+    SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(4), &m_spritesPositions.at(5));
+    SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(4), &m_spritesPositions.at(6));
+    SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(5), &m_spritesPositions.at(7));
+
+    for (const SDL_FRect &dstRect : m_spritesPositions)
+    {
+    }
     for (std::size_t i {}; i < 4; ++i)
     {
-        SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(i), &m_spritesPosition.at(i));
+        SDL_RenderTexture(renderer, m_sprites.get(), &m_spritesCut.at(i), &m_spritesPositions.at(i));
     }
 }
